09843ab2674272df3d2da7f725ade9a6ebbd4d7f
ryuhigo <ryu_higo@icloud.com>
2022-04-30 11:04:06 +0900

    create driving manager

---
+++ driving_manager.cpp
@@ -0,0 +1,88 @@
+#include "driving_manager.h"
+
+DrivingManager::DrivingManager(BasicDriver* basic_driver, LineTracer* line_tracer, EndCondition* end_condition)
+    : basic_driver_(basic_driver), line_tracer_(line_tracer), end_condition_(end_condition) {
+}
+
+void DrivingManager::Update() {
+  if (driving_params_.size() <= 0) {
+    return;
+  }
+
+  DrivingParam& curr_param = driving_params_.front();
+  if (!curr_param.is_started) {
+    SetMoveParam(curr_param);
+    SetEndParam(curr_param);
+    curr_param.is_started = true;
+  }
+
+  Drive(curr_param);
+  if (end_condition_->IsSatisfied()) {
+    driving_params_.pop_front();
+  }
+
+  if (driving_params_.empty()) {
+    basic_driver_->Stop();
+  }
+}
+
+void DrivingManager::AddDrivingParam(DrivingParam param) {
+  driving_params_.push_back(param);
+}
+
+bool DrivingManager::DrivingParamsEmpty() {
+  return driving_params_.empty();
+}
+
+void DrivingManager::SetMoveParam(DrivingParam& param) {
+  Move move_type = param.move_type;
+  int8_t ref_power = param.ref_power;
+  float ref_value = param.ref_value;
+  Gain gain = param.gain;
+
+  switch (move_type) {
+    case kTraceLeftEdge:
+    case kTraceRightEdge:
+      line_tracer_->SetParam(move_type, ref_power, ref_value, gain);
+      break;
+
+    case kGoForward:
+    case kGoBackward:
+    case kRotateLeft:
+    case kRotateRight:
+      basic_driver_->SetParam(move_type, ref_power);
+      break;
+
+    default:
+      break;
+  }
+}
+
+void DrivingManager::SetEndParam(DrivingParam& param) {
+  End end_type = param.end_type;
+  Color end_color = param.end_color;
+  float end_threshold = param.end_threshold;
+
+  end_condition_->SetParam(end_type, end_color, end_threshold);
+}
+
+void DrivingManager::Drive(DrivingParam& param) {
+  Move move_type = param.move_type;
+
+  switch (move_type) {
+    case kTraceLeftEdge:
+    case kTraceRightEdge:
+      line_tracer_->Run();
+      break;
+
+    case kGoForward:
+    case kGoBackward:
+    case kRotateLeft:
+    case kRotateRight:
+      basic_driver_->Run();
+      break;
+
+    default:
+      break;
+  }
+}
---
+++ driving_manager.h
@@ -0,0 +1,28 @@
+#ifndef ETRC22_DRIVING_MANAGER_H_
+#define ETRC22_DRIVING_MANAGER_H_
+
+#include "driving_type.h"
+#include "end_condition.h"
+#include "info_type.h"
+#include "utils.h"
+
+#include <list>
+
+class DrivingManager {
+ public:
+  DrivingManager(BasicDriver* basic_driver, LineTracer* line_tracer, EndCondition* end_condition);
+  void Update();
+  void AddDrivingParam(DrivingParam param);
+  bool DrivingParamsEmpty();
+
+ private:
+  void SetMoveParam(DrivingParam& param);
+  void SetEndParam(DrivingParam& param);
+  void Drive(DrivingParam& param);
+  BasicDriver* basic_driver_;
+  LineTracer* line_tracer_;
+  EndCondition* end_condition_;
+  std::list<DrivingParam> driving_params_;
+};
+
+#endif  // ETRC22_DRIVING_MANAGER_H_
